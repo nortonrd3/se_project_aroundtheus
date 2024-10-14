fb4c563962aaf013ff716e4cd9056f4d700223c2
Robert Norton <robertnorton913@gmail.com>
2024-10-14 16:38:11 +0200

    Add methods to API class

--- src/components/Api.js
+++ src/components/Api.js
@@ -26,4 +26,52 @@ export default class Api {
   getAllData() {
     return Promise.all([this.getInitialCards(), this.getUserInfo()]);
   }
+
+  updateUserInfo(userData) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: userData.title,
+        about: userData.description,
+      }),
+    }).then(this._handlePromise);
+  }
+
+  updateAvatar(link) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify(link),
+    }).then(this._handlePromise);
+  }
+
+  addLike(Id) {
+    return fetch(`${this._baseUrl}/cards/${Id}/likes`, {
+      method: "PUT",
+      headers: this._headers,
+    }).then(this._handlePromise);
+  }
+
+  removeLike(Id) {
+    return fetch(`${this._baseUrl}/cards/${Id}/likes`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._handlePromise);
+  }
+
+  addCard({ name, link, _id }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({ name, link, _id }),
+    }).then(this._handlePromise);
+  }
+
+  deleteCard(Id) {
+    return fetch(`${this._baseUrl}/cards/${Id}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._handlePromise);
+  }
 }
